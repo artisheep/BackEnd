f5651d6310b0cd4e063a2d6f63f21a8440154956
seongguk-Kim <kjk7212@gmail.com>
2023-07-12 09:25:20 +0900

    properties

--- src/main/java/com/swave/releasenotesharesystem/ReleaseNote/repository/CommentCustomRetository.java
+++ src/main/java/com/swave/releasenotesharesystem/ReleaseNote/repository/CommentCustomRetository.java
@@ -5,4 +5,5 @@ import com.swave.releasenotesharesystem.ReleaseNote.domain.Comment;
 import java.util.ArrayList;
 
 public interface CommentCustomRetository {
+    ArrayList<Comment> findTop5RecentComment(Long projectId);
 }
