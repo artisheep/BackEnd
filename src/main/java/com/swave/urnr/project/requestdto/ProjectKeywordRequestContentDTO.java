0ff167b2bf7a42af21d69cd1b0c935c40cabc031
cmilk18 <43445697+cmilk18@users.noreply.github.com>
2023-07-26 16:03:29 +0900

    0726 DTO 업데이트
    DTO설명추가

--- src/main/java/com/swave/urnr/project/requestdto/ProjectKeywordRequestContentDTO.java
+++ src/main/java/com/swave/urnr/project/requestdto/ProjectKeywordRequestContentDTO.java
@@ -1,8 +1,13 @@
 package com.swave.urnr.project.requestdto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel(value = "프로젝트 검색 DTO")
 public class ProjectKeywordRequestContentDTO {
+    
+    @ApiModelProperty(value="검색 키워드 입력", example = "Wave Form", required = true)
     String keyword;
 }
